6e5960ef35ad8d7f9857a3c936598d29bc519fc1
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 00:17:40 -0700

    Instructor note

--- mktmpfiles.sh
+++ mktmpfiles.sh
@@ -5,3 +5,16 @@ cd testfiles
 for i in $(seq -w 100); do mkdir "file$i.tmp";done
 cd -
 exit 0
+
+###
+# INSTRUCTOR NOTE:
+#
+# The construction:
+#   for ... ; do ...; done
+# is the bash syntax for the for loop.
+# "do" is a needed keyword simply to complete this triplet.
+#
+# See also:
+#   if ...; then ...; fi
+# as a similar syntax triplet.
+###
